c9d656a1dbb7f137932c630ce62b76ba128dadd4
Max <mirms7@tutanota.com>
2022-02-26 15:51:38 +0300

    created 07_shop_display_product_discount.py

--- 07_shop_display_product_discount.py
+++ 07_shop_display_product_discount.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC_click
 from selenium.webdriver.support.select import Select
 # ---------------------------------------------------------------------------- #
 #                   *   Loading Chrome Profile for testing   *                 #
@@ -32,6 +34,39 @@ reg_btn.click()
 #                             3. Click "Shop" Link                             #
 # ---------------------------------------------------------------------------- #
 driver.find_element(By.LINK_TEXT, "Shop").click()
-
+# ---------------------------------------------------------------------------- #
+#                   4. Open book "Android Quick Start Guide"                   #
+# ---------------------------------------------------------------------------- #
+driver.find_element(
+    By.XPATH, "//h3[text() ='Android Quick Start Guide']").click()
+# ---------------------------------------------------------------------------- #
+#                        5. Assert Old Price of the book                       #
+# ---------------------------------------------------------------------------- #
+old_price_exp = "₹600.00"
+old_price = driver.find_element(By.XPATH, "//del/span")
+print("--- 5 Step (assert Old Price) ---")
+print("The Old Price Rusult:", old_price.text,
+      "\nExpected Old Price Result:", old_price_exp)
+print("---------------------------------")
+assert old_price.text == old_price_exp
+# ---------------------------------------------------------------------------- #
+#                        6. Assert New Price of the book                       #
+# ---------------------------------------------------------------------------- #
+new_price_exp = "₹450.00"
+new_price = driver.find_element(By.XPATH, "//ins/span")
+print("--- 6 Step (assert New Price) ---")
+print("The New Price Rusult:", new_price.text,
+      "\nExpected New Price Result:", new_price_exp)
+print("---------------------------------")
+assert new_price.text == new_price_exp
+# ---------------------------------------------------------------------------- #
+#                     Book Cover Image (Explicit  Waiting)                     #
+# ---------------------------------------------------------------------------- #
+wait = WebDriverWait(driver, 10).until
+EC_click = EC_click.element_to_be_clickable
+# >---------------------- 7. Click on Book cover image ----------------------- #
+wait(EC_click((By.CLASS_NAME, "wp-post-image"))).click()
+# >---------------------- 8. Close the Book cover image ---------------------- #
+wait(EC_click((By.CLASS_NAME, "pp_close"))).click()
 # ---------------------------------------------------------------------------- #
 driver.quit()
